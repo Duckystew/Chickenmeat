7375f5974f65544e6f75127b2047f3679351b2b2
Ankpudding <ankpudding@outlook.com>
2025-12-12 21:47:31 +0100

    Added recipe for chicken nuggets. Updated to 1.21.11. Updated art for chicken nuggets.

--- src/main/java/com/ankpudding/duckystew/chickenmeat/ModItems.java
+++ src/main/java/com/ankpudding/duckystew/chickenmeat/ModItems.java
@@ -1,18 +1,16 @@
 package com.ankpudding.duckystew.chickenmeat;
 
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.item.component.Consumables;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
@@ -22,24 +20,28 @@ import java.util.function.Function;
 
 public class ModItems{
     public static final Consumable CHICKEN_NUGGET_COMPONENT = Consumables.defaultFood()
-            .onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.LEVITATION, 6 * 20, 2), 1.0f))
+            //Add slow falling effect and change consume time to 0.8 seconds
+            .onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 20, 1), 1.0f))
             .build();
 
     public static final Item CHICKEN_NUGGET = register(
             "chicken_nugget",
             Item::new,
-            new Item.Properties().rarity(Rarity.EPIC).food(new FoodProperties.Builder()
-                            .nutrition(10)
-                            .alwaysEdible()
-                            .saturationModifier(10)
+            new Item.Properties()
+                    //Item max stack size
+                    .stacksTo(16)
 
+                    //Add food nutrition
+                    .food(new FoodProperties.Builder()
+                            .nutrition(8)
+                            .saturationModifier(12.8f)
                     .build()
             ,CHICKEN_NUGGET_COMPONENT)
     );
 
     public static Item register(String name, Function<Item.Properties, Item> itemFactory, Item.Properties settings) {
         // Create the item key.
-        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Chickenmeat.MOD_ID, name));
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Chickenmeat.MOD_ID, name));
 
         // Create the item instance.
         Item item = itemFactory.apply(settings.setId(itemKey));
